4801c65235c273ee51d07f9e3b821828d121dfd7
vin070 <34207176+vin070@users.noreply.github.com>
2022-07-06 18:07:03 +0530

    Update
    Easily configurable and uploaded at multiple domain at once.

--- updateBuild.bash
+++ updateBuild.bash
@@ -1,24 +1,27 @@
 #!/bin/bash
-buildUploadPath='/var/www/html'
-function checkError(){
+build_update_path=$1;
+build_upload_path=$2; 
+function check_error(){
   if [[ $? != 0 ]]
    then
      echo 'ERROR'
      exit
    fi 
-} 
+}
 echo '---SSH done to server---'
-tar -xJf dist.tar.xz --directory=${buildUploadPath}
-echo "--- build extracted at ${buildUploadPath}---"
-checkError
-cd ${buildUploadPath}
-checkError
+cd ${build_upload_path}
+tar -xJf dist.tar.xz --directory=.
+echo "---build extracted at ${build_upload_path} directory"
+check_error
+cd ${build_update_path}
+check_error
 sudo rm -rf  *.js *.svg *.png *.woff *.eot *.ttf *.png *.css  assets *.html *.ico *.txt
-echo "---previous build removed from ${buildUploadPath} path---"
-checkError
-sudo cp -r dist/* .
-echo "---new build copied at ${buildUploadPath} path---"
-checkError
-sudo rm -rf dist
-checkError
-echo "---extracted dist folder removed---"
+echo "---previous build removed from ${build_update_path} path SUBJECT TO AVAILABILITY---"
+check_error
+sudo cp -r ${build_upload_path}/dist/* ${build_update_path}/
+echo "---new build copied at ${build_update_path} path---"
+check_error
+cd ${build_upload_path}
+sudo rm -rf dist dist.tar.xz
+check_error
+echo "---uploaded and extracted dist folder removed---"
